f1ee24ab752208c775a1aadf1e27794e587ab802
Ayberk <kenan.ayberk@hotmail.com>
2024-08-15 10:09:15 +0300

    pairProcessor eklendi

---
+++ pairProcessor/InputHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace pairProcessor
+{
+    public class InputHandler
+    {
+        public int GetNumberOfPairs()
+        {
+            Console.WriteLine("Kaç sayı çifti gireceğinizi belirtiniz:");
+            string? input = Console.ReadLine();
+
+            int numberOfPairs;
+            while (string.IsNullOrWhiteSpace(input) || !int.TryParse(input, out numberOfPairs))
+            {
+                Console.WriteLine("Geçersiz giriş. Lütfen pozitif bir tam sayı girin!");
+                input = Console.ReadLine();
+            }
+
+            return numberOfPairs;
+        }
+
+        public (int, int) GetPair(int pairNumber)
+        {
+            Console.WriteLine($"Lütfen {pairNumber}. sayı çiftini giriniz (örnek: 1 2):");
+            string? input = Console.ReadLine();
+
+            while (string.IsNullOrWhiteSpace(input) || input.Split(' ').Length != 2)
+            {
+                Console.WriteLine("Geçersiz giriş. Lütfen iki sayı giriniz (örnek: 1 2):");
+                input = Console.ReadLine();
+            }
+
+            string[] numbers = input.Split(' ');
+
+            while (!int.TryParse(numbers[0], out int num1) || !int.TryParse(numbers[1], out int num2))
+            {
+                Console.WriteLine("Geçersiz giriş. Lütfen geçerli iki sayı giriniz (örnek: 1 2):");
+                input = Console.ReadLine();
+                numbers = input?.Split(' ') ?? new string[0];
+            }
+
+            return (int.Parse(numbers[0]), int.Parse(numbers[1]));
+        }
+    }
+}
---
+++ pairProcessor/Program.cs
@@ -0,0 +1,19 @@
+﻿using pairProcessor;
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        InputHandler inputHandler = new InputHandler();
+        PairProcessor pairProcessor = new PairProcessor();
+        int n = inputHandler.GetNumberOfPairs();
+
+        for (int i = 0; i < n; i++)
+        {
+            var (num1, num2) = inputHandler.GetPair(i + 1);
+            int result = pairProcessor.ProcessorPair(num1, num2);
+            Console.WriteLine($"Sonuç: {result}");
+        }
+    }
+}
